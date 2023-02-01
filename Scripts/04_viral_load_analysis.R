211631425cc9b8e8c3985b86dae1a4d439a918bf
tessam30 <tim.essam@gmail.com>
2023-02-01 14:42:18 +0200

    feat: add in viral load age analysis by snu1

--- Scripts/04_viral_load_analysis.R
+++ Scripts/04_viral_load_analysis.R
@@ -236,3 +236,129 @@ df_genie %>% count(funding_agency)
       
       si_save("Graphics/VL_summary_partners.svg")
       
+
+# VIRAL LOAD BY PROVINCES -------------------------------------------------
+      
+      df_vl_snu1 <- df_genie %>% 
+        filter(funding_agency == "USAID",
+               indicator %in% c("TX_CURR","TX_CURR_Lag2", "TX_PVLS"),
+               standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "Age/Sex/Indication/HIVStatus")) %>% 
+        clean_indicator() %>% 
+        group_by(snu1, indicator, fiscal_year) %>% 
+        summarise(across(starts_with("qtr"), sum, na.rm = TRUE), .groups = "drop") %>% 
+        reshape_msd(include_type = FALSE) %>% 
+        pivot_wider(names_from = indicator,
+                    names_glue = "{tolower(indicator)}") %>% 
+        arrange(snu1, period) %>% 
+        group_by(snu1) %>% 
+        mutate(tx_curr_lag2 = lag(tx_curr, n = 2),
+               vlc = tx_pvls_d/tx_curr_lag2,
+               vls = tx_pvls / tx_pvls_d,
+               vls_adj = tx_pvls /tx_curr_lag2) %>% 
+        ungroup()
+      
+      
+      df_vl_snu1 %>% 
+        filter(snu1 != "Lusaka", 
+               str_detect(period, "20", negate = T)) %>% 
+        ggplot(aes(x = period, group = 1)) +
+        geom_line(aes(y = vls), color = burnt_sienna) +
+        geom_point(aes(y = vls), shape = 21, fill = burnt_sienna, size = 3,
+                   color = "white") +
+        geom_line(aes(y = vlc), color = denim) +
+        geom_point(aes(y = vlc), shape = 21, fill = denim, size = 3,
+                   color = "white") +
+        geom_text(aes(y = vlc, label = percent(vlc, 1)), size = 9/.pt,
+                  family = "Source Sans Pro", color = denim, 
+                  vjust = -1) +
+        geom_text(aes(y = vls, label = percent(vls, 1)), size = 9/.pt,
+                  family = "Source Sans Pro", color = burnt_sienna, 
+                  vjust = -1) +
+        facet_wrap(~snu1) +
+        #scale_x_discrete(breaks = every_nth(n = 4)) +
+        si_style_nolines(facet_space = 0.5) +
+        expand_limits(x = c(1, 10), y = c(0.7,1.05)) +
+        theme(axis.text.y = element_blank()) +
+        labs(x = NULL, y = NULL) +
+        labs(title = "USAID VIRAL LOAD COVERAGE AND VIRAL LOAD SUPPRESSION TRENDS", 
+             caption = glue("{metadata$caption}"))
+      si_save("Graphics/ZMB_vls_vlc_by_snu1.svg")      
+      
+# VIRAL LOAD BY AGE/SEX -------------------------------------------------
+      
+      df_vl <- df_genie %>% 
+        filter(funding_agency == "USAID",
+               indicator %in% c("TX_CURR","TX_CURR_Lag2", "TX_PVLS"),
+               standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "Age/Sex/Indication/HIVStatus"),
+               ageasentered != "Unknown Age") %>% 
+        mutate(ageasentered = case_when(trendscoarse == "<15" ~ trendscoarse,
+                                        ageasentered %in% c("50-54","55-59", "60-64", "65+") ~ "50+",
+                                        TRUE ~ ageasentered)) %>% 
+        clean_indicator() %>% 
+        group_by(snu1, indicator, ageasentered, fiscal_year) %>% 
+        summarise(across(starts_with("qtr"), sum, na.rm = TRUE), .groups = "drop") %>% 
+        reshape_msd(include_type = FALSE) %>% 
+        pivot_wider(names_from = indicator,
+                    names_glue = "{tolower(indicator)}")
+      
+      df_vl <- df_vl %>% 
+        arrange(snu1, ageasentered, period) %>% 
+        group_by(snu1, ageasentered) %>% 
+        mutate(tx_curr_lag2 = lag(tx_curr, n = 2),
+               vlc = tx_pvls_d/tx_curr_lag2,
+               vls = tx_pvls / tx_pvls_d,
+               vls_adj = tx_pvls /tx_curr_lag2) %>% 
+        ungroup() %>% 
+        filter(period == max(period))
+      
+      df_avg <- df_vl %>% 
+        group_by(snu1) %>% 
+        summarise(avg_vlc = mean(vlc),
+                  avg_vls = mean(vls),
+                  avg_vls_adj = mean(vls_adj)) %>% 
+        ungroup()
+      
+      
+      df_usaid_vl <- df_vl %>% 
+        summarise(vlc = sum(tx_pvls_d, na.rm = TRUE)/sum(tx_curr_lag2, na.rm = TRUE),
+                  vls = sum(tx_pvls, na.rm = TRUE) / sum(tx_pvls_d, na.rm = TRUE),
+                  vls_adj = sum(tx_pvls, na.rm = TRUE) /sum(tx_curr_lag2, na.rm = TRUE))
+      
+      
+      df_vl %>% 
+        mutate(tx_curr_pct = 1) %>% 
+        ggplot(aes(tx_curr_pct, ageasentered)) +
+        geom_vline(data = df_avg, aes(xintercept = avg_vlc, color = grey80k)) +
+        geom_col(fill = grey10k, alpha = 0.6) +
+        geom_col(aes(vls), fill = scooter_light, alpha = 0.75, width = 0.75) +
+        geom_col(aes(vlc), fill = scooter, alpha = 1, width = 0.75) +
+        geom_vline(xintercept = 1, linewidth = 0.5, color = grey30k) +
+        geom_vline(xintercept = 0, linewidth = 0.5, color = grey30k) +
+        geom_richtext(aes(vls, label = glue("<span style='color:#505050'>{percent(vls, 1)}</span>")),
+                      label.color = NA, fill = NA,
+                      nudge_x = -0.04, size = 3,
+                      family = "Source Sans Pro") +
+        geom_richtext(aes(vlc, label = glue("<span style='color:white'>{percent(vlc, 1)}</span>")),
+                      label.color = NA, fill = NA,
+                      nudge_x = -0.04, size = 3,
+                      family = "Source Sans Pro") +
+        geom_text(data = df_avg, aes(x = avg_vlc, y = avg_vlc, label = percent(avg_vlc, 1)),
+                  hjust = .3, vjust = 1.6,
+                  family = "Source Sans Pro",
+                  color = grey90k, size = 10/.pt) +
+        facet_wrap(~snu1) +
+        coord_cartesian(expand = T, clip = "off") +
+        scale_color_identity() +
+        scale_x_continuous(label = percent) +
+        labs(x = NULL, y = NULL, 
+             title = glue("In {metadata$curr_pd}, USAID VLC and VLS are at {percent(df_usaid_vl$vlc, 1)} and {percent(df_usaid_vl$vls, 1)} respectively") %>% toupper,
+             subtitle = glue("<span style='color:{scooter}'>**VLC**</span> and <span style='color:{scooter_light}'>**VLS**</span> rates"),
+             caption = glue("Source: {metadata$source}")) +
+        si_style_nolines() +
+        theme(panel.spacing = unit(.5, "line"),
+              plot.subtitle = element_markdown(),
+              strip.text = element_markdown(),
+              axis.text.x = element_blank()) 
+      
+      si_save("Graphics/VLC_VLS_by_province.svg")
+      
