4b3de2a70dad619d590fde26d2ab515d96aa55b4
tessam30 <tim.essam@gmail.com>
2023-04-18 16:07:20 -0400

    feat: update datapack review tables

--- Scripts/200_Taming_DP_draft1_2023_03-27.R
+++ Scripts/200_Taming_DP_draft1_2023_03-27.R
@@ -24,7 +24,8 @@
     load_secrets()
     merdata <- file.path(glamr::si_path("path_msd"))
     msd_path <- msd_path <- return_latest(merdata, "PSNU_IM_FY21-23_20230210.*Zambia")
-    dp_path <- "Data/2023-03-27 Target Setting Tool_For Check point 1 1812.xlsx"  
+    #dp_path <- "Data/2023-03-27 Target Setting Tool_For Check point 1 1812.xlsx"  
+    dp_path <- "../../../Downloads/2023-04-14 Target Setting Tool_5PM.xlsx"
     
   # Grab metadata
    get_metadata(msd_path)
@@ -52,19 +53,24 @@
     mutate(diff = `2024` - `2023`,
            delta = ((`2024`/`2023`) - 1) %>% percent(., 1))
   
-  #write_csv(dp_tgs, "Dataout/dp_2023_03_28_check.csv")
+  gs_id <- "1gQioul3yS1o92OAqEVGU6vFjlt4MkNQTqmy9TxxAug0"
+  googlesheets4::sheet_write(dp_tgs, ss = gs_id, sheet = 3)
 
  msd %>% 
-   filter(indicator == "HTS_TST", fiscal_year == 2023) %>% 
+   filter(indicator == "TX_CURR", fiscal_year == 2023) %>% 
    group_by(standardizeddisaggregate) %>% 
    summarise(targets = sum(targets, na.rm = T), .groups = "drop")
  
  
+ snu1_cw <- msd %>% distinct(snu1, psnu, psnuuid)
+ 
  dp_plhiv %>% 
+   left_join(., snu1_cw) %>% 
    clean_indicator() %>% 
-   group_by(indicator, fiscal_year, standardizeddisaggregate, snu1) %>% 
+   group_by(indicator, fiscal_year, standardizeddisaggregate) %>% 
    summarize(targets = sum(targets, na.rm =T)) %>% 
-   spread(fiscal_year, targets)
+   spread(fiscal_year, targets) %>% 
+   filter(indicator == "PLHIV")
   
 # VIZ ============================================================================
 
